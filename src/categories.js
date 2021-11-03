5b6d5448337a3e4d80be3186ae93ff98e1e32b12
rleibmann <ruben.leibmann@cofinpro.de>
2021-11-03 17:10:41 +0100

    Added SVG-Renderer

---
+++ src/categories.js
@@ -0,0 +1,44 @@
+module.exports = {
+    categories: [
+        {
+            "name": "PERSON",
+            "symbol": 'roundrect',
+            "symbolSize": 30,
+            "itemStyle": {
+                "color": 'blue',
+                "borderColor": 'black',
+                "borderWidth": 2
+            }
+        },
+        {
+            "name": "COMPANY",
+            "symbol": 'roundrect',
+            "symbolSize": 30,
+            "itemStyle": {
+                "color": 'orange',
+                "borderColor": 'black',
+                "borderWidth": 2
+            }
+        },
+        {
+            "name": "PUBLIC",
+            "symbol": 'roundrect',
+            "symbolSize": 30,
+            "itemStyle": {
+                "color": 'green',
+                "borderColor": 'black',
+                "borderWidth": 2
+            }
+        },
+        {
+            "name": "ROOT",
+            "symbol": 'roundrect',
+            "symbolSize": 40,
+            "itemStyle": {
+                "color": 'red',
+                "borderColor": 'black',
+                "borderWidth": 2
+            }
+        }
+    ],
+}
